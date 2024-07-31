2cad6c74567428cb354ac2c965a9891faacad90e
MoitreyoGhosh <moitreyoghosh@gmail.com>
2024-07-31 23:48:31 +0530

    Routing and Folder Structure Set Up

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+//localhost:3000/transformations wrong X
+//localhost:3000/transformations/[id] ,right!
---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
+});
+
+export const metadata: Metadata = {
+  title: "Imaginify",
+  description: "AI powered image generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+        {children}
+      </body>
+    </html>
+  );
+}
