fca13d77247d444c755217ba640874a0180547c3
MoitreyoGhosh <moitreyoghosh@gmail.com>
2024-08-03 21:02:47 +0530

    implement clerk webhooks

--- middleware.tsx
+++ middleware.tsx
@@ -1,6 +1,6 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
@@ -10,3 +10,4 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 };
+
