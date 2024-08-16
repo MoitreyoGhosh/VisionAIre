1e4ef4b748bad11e67929fc206f182828e7edc01
MoitreyoGhosh <moitreyoghosh@gmail.com>
2024-08-16 20:14:46 +0530

    Updated file

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,7 @@
-import { SignIn, useUser } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
 import React from "react";
 
 const SignInPage = () => {
-  const { isSignedIn } = useUser();
-
-  if (isSignedIn) return null;
-
   return (
     <SignIn
       forceRedirectUrl={"/"}
